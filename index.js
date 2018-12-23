3a13c23c878dccf449bfe2d67c3069a0bc33ae0a
moonRachel <43541851+moonRachel@users.noreply.github.com>
2018-12-23 15:35:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+var pics = new Vue({
+    		el:"#clothes",
+    		data:{
+    			pics:[
+    				{
+    					clothes:"clothes1",
+    					img:"images/淘宝图1.png",
+    					money:"￥199.00",
+    					content:"拍下8折熙世界旗舰店2018秋季新款",
+    					shop:"熙世界青铜专卖店",
+    					a:"如实评价：4.8",
+    					b:"服务态度：4.8",
+    					c:"发货速度：4.8",
+
+    				},
+    				{
+    					clothes:"clothes2",
+    					img:"images/淘宝2.png",
+    					money:"￥199.00",
+    					content:"拍下8折熙世界旗舰店2018秋季新款",
+    					shop:"熙世界青铜专卖店",
+    					a:"如实评价：4.0",
+    					b:"服务态度：4.0",
+    					c:"发货速度：4.0",
+
+    				},
+    				{
+    					clothes:"clothes3",
+    					img:"images/淘宝3.png",
+    					money:"￥198.00",
+    					content:"2018春装女新款碎花雪纺连衣裙春夏印花",
+    					shop:"lababite官方旗舰店",
+    					a:"如实评价：4.8",
+    					b:"服务态度：4.8",
+    					c:"发货速度：4.8",
+
+    				},
+    				{
+    					clothes:"clothes4",
+    					img:"images/淘宝4.png",
+    					money:"￥199.00",
+    					content:"女款新牛仔针织长款修身九分连衣裙",
+    					shop:"灰太狼2294",
+    					a:"如实评价：4.7",
+    					b:"服务态度：4.8",
+    					c:"发货速度：4.8",
+
+    				},
+    				{
+    					clothes:"clothes5",
+    					img:"images/淘宝5.png",
+    					money:"￥129.00",
+    					content:"艾思林2018新款女装复古印花雪纺荷叶边",
+    					shop:"艾思林旗舰店",
+    					a:"如实评价：4.9",
+    					b:"服务态度：4.8",
+    					c:"发货速度：4.8",
+
+    				},
+    				{
+    					clothes:"clothes6",
+    					img:"images/淘宝6.png",
+    					money:"￥228.00",
+    					content:"明星同款一字领露肩蓝色蕾丝修身显瘦",
+    					shop:"江米团猫",
+    					a:"如实评价：4.9",
+    					b:"服务态度：4.8",
+    					c:"发货速度：4.8",
+
+    				}
+
+    			]
+    		}
+    	});
